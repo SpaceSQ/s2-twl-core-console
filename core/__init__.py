d499671dda2b10819c89a854cbed4fe812ff28ee
MileXiang <smarthomemiles@gmail.com>
2026-04-06 15:00:36 +0800

    Delete core directory

--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-# S2-SWM Indoor Governance Core Package
-from .visa_manager import SpatioTemporalVisaManager
-from .spatial_ledger import SpatialLedger
-from .lord_brain import LordGovernanceBrain
-
-__all__ = ["SpatioTemporalVisaManager", "SpatialLedger", "LordGovernanceBrain"]
